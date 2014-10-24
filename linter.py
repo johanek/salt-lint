4b4ee580c291308d4f4056b65aa2ec357e8e9c7a
Johan van den Dorpe <johan@johan.org.uk>
2014-10-24 11:49:25 +0100

    Add slscheck function using salt yaml renderer

--- linter.py
+++ linter.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+
 import sys
 import yaml
 import re
+import salt.config
+import salt.loader
+from inspect import getargspec
+import importlib
 
 def tabcheck(filename):
 	
@@ -29,7 +34,7 @@ def indentcheck(filename):
       current = len(line) - len(line.lstrip())
 
       if current > previous:
-        tabsize = current - previous  
+        tabsize = current - previous
 
         if nested == 1:
           if tabsize != 4:
@@ -38,7 +43,7 @@ def indentcheck(filename):
         if tabsize not in [0, required]:
           ParseError(filename, count, 'Two space soft tabs not found - %s space tab found' % tabsize)
 
-      """ context and default options are nested dicts - need 4 space tabs 
+      """ context and default options are nested dicts - need 4 space tabs
           http://docs.saltstack.com/en/latest/topics/troubleshooting/yaml_idiosyncrasies.html """
       match = re.search('context|defaults:', line)
 
@@ -51,7 +56,7 @@ def indentcheck(filename):
       previous = current
 
 
-def lint(filename):
+def yamlcheck(filename):
 
   with open(filename) as file:
     try:
@@ -67,6 +72,28 @@ def lint(filename):
       ParseError(filename, 0, 'Unexpected Error parsing YAML')
       return 2
 
+
+def slscheck(filename):
+  content = open(filename).read()
+  __opts__ = salt.config.minion_config('minion.conf')
+  renderers = salt.loader.render(__opts__, {})
+  data = renderers['yaml'](content)
+
+  for id, v in data.items():
+    for state, options in v.items():
+      package = importlib.import_module("salt.states.%s" % state)
+      method = options[0]
+
+      try:
+        args = getargspec(getattr(package, method)).args
+        for opt in options[1:]:
+          option = opt.keys()[0]
+          if option not in args:
+            ParseError(filename, 0, '%s state with id \'%s\' calls unknown option %s' % (state, id, option ))
+      except:
+        ParseError(filename, 0, '%s state with id \'%s\' calls unknown method %s' % (state, id, method))
+
+
 def ParseError(filename, line, message):
   ERRORS.append({'filename':filename,'line':line,'message':message})
 
@@ -75,11 +102,15 @@ if __name__ == '__main__':
   ERRORS = []
 
   tabcheck(sys.argv[1])
-  lint(sys.argv[1])
   indentcheck(sys.argv[1])
+  #yamlcheck(sys.argv[1])
+  slscheck(sys.argv[1])
 
   for err in sorted(ERRORS, key = lambda e: (e['filename'], e['line'])):
-    print "%s:%s - %s" % (err['filename'], err['line'], err['message'])
+    if err['line'] == 0:
+      print "%s - %s" % (err['filename'], err['message'])
+    else:
+      print "%s:%s - %s" % (err['filename'], err['line'], err['message'])
 
   if len(ERRORS) == 0:
     sys.exit(0)
